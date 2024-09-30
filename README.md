638021956bf66d1be88922a2b442583d877d468b
rxshabN <nagwanirishab@gmail.com>
2024-10-01 00:45:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Password-Manager---mongodb
+ Password Manager which is connected to mongodb database via ejs backend server
---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { MongoClient } = require("mongodb");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+
+dotenv.config();
+
+const url = "mongodb://localhost:27017/";
+const client = new MongoClient(url);
+
+const dbName = "PasswordManager";
+const app = express();
+const port = 3000;
+app.use(bodyparser.json());
+app.use(cors());
+
+client.connect();
+
+app.get("/", async (req, res) => {
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection("passwords");
+  const findResult = await collection.find({}).toArray();
+  res.json(findResult);
+});
+
+app.post("/", async (req, res) => {
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection("passwords");
+  const findResult = await collection.insertOne(password);
+  res.send({ success: true, result: findResult });
+});
+
+app.delete("/", async (req, res) => {
+  const password = req.body;
+  const db = client.db(dbName);
+  const collection = db.collection("passwords");
+  const findResult = await collection.deleteOne(password);
+  res.send({ success: true, result: findResult });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
